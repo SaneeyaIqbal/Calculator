f9e1fc76734b14501f305287996464b964ed6f06
SaneeyaIqbal <s.saneeya.iqbal@gmail.com>
2019-11-27 17:39:50 +0530

    removed function

--- calci/src/App.js
+++ calci/src/App.js
@@ -19,107 +19,120 @@ class Calculate extends Component {
     this.Eight=this.Eight.bind(this);
     this.Nine=this.Nine.bind(this);
     this.Zero=this.Zero.bind(this);
-    //this.takeQuantity=this.takeQuantity.bind(this);
+    this.takeQuantity=this.takeQuantity.bind(this);
   }
 
-    One (){
-        this.setState(
-          {
-            item:this.state.item  +'1'
-          }
-        );
-      }
+    // One (){
+    //     this.setState(
+    //       {
+    //         item:this.state.item  +'1'
+    //       }
+    //     );
+    //   }
     
-    Two (){
-        this.setState(
-          {
-            item:this.state.item + '2'
-          }
-        );
-      }
+    // Two (){
+    //     this.setState(
+    //       {
+    //         item:this.state.item + '2'
+    //       }
+    //     );
+    //   }
     
 
-    Three (){
-        this.setState(
-          {
-            item:this.state.item +'3'
-          }
-        );
-      }
+    // Three (){
+    //     this.setState(
+    //       {
+    //         item:this.state.item +'3'
+    //       }
+    //     );
+    //   }
 
-    Four(){
-        this.setState(
-          {
-            item:this.state.item + '4'
-          }
-        );
-      }
+    // Four(){
+    //     this.setState(
+    //       {
+    //         item:this.state.item + '4'
+    //       }
+    //     );
+    //   }
     
     
-    Five(){
-        this.setState(
-          {
-            item:this.state.item + '5'
-          }
-        );
-      }
+    // Five(){
+    //     this.setState(
+    //       {
+    //         item:this.state.item + '5'
+    //       }
+    //     );
+    //   }
     
-    Six(){
-        this.setState(
-          {
-            item:this.state.item + '6'
-          }
-        );
-      }
+    // Six(){
+    //     this.setState(
+    //       {
+    //         item:this.state.item + '6'
+    //       }
+    //     );
+    //   }
 
-    Seven(){
-        this.setState(
-          {
-            item:this.state.item + '7'
-          }
-        );
-      }
+    // Seven(){
+    //     this.setState(
+    //       {
+    //         item:this.state.item + '7'
+    //       }
+    //     );
+    //   }
     
-    Eight(){
-        this.setState(
-          {
-            item:this.state.item + '8'
-          }
-        );
-      }
+    // Eight(){
+    //     this.setState(
+    //       {
+    //         item:this.state.item + '8'
+    //       }
+    //     );
+    //   }
     
     
-    Nine(){
-        this.setState(
-          {
-            item:this.state.item + '9'
-          }
-        );
-      }
+    // Nine(){
+    //     this.setState(
+    //       {
+    //         item:this.state.item + '9'
+    //       }
+    //     );
+    //   }
     
-    Zero(){
-        this.setState(
-          {
-            item:this.state.item + '0'
-          }
-        );
+    // Zero(){
+    //     this.setState(
+    //       {
+    //         item:this.state.item + '0'
+    //       }
+    //     );
+    //   }
+
+      takeQuantity(event){
+        let item = event.target.value;
+        if (item){
+          this.setState({
+            item:parseInt(item)
+          });
+        }
+      
+      
       }
 
   render()  {
     return (
         <div className="name">
-          <input className="input" type="text" value={this.state.item}/>
+          <input className="input" type="text" onChange={this.takeQuantity} value={this.state.item}/>
           <div className="name">
-            <button onClick={this.Nine} className = "button" type="button">9</button>
-            <button onClick={this.Eight} className = "button">8</button>
-            <button onClick={this.Seven} className = "button">7</button><br />
-            <button onClick={this.Six} className = "button">6</button>
-            <button onClick={this.Five} className = "button">5</button>
-            <button onClick={this.Four } className = "button">4</button><br />
-            <button onClick={this.Three} className = "button">3</button>
-            <button onClick={this.Two} className = "button">2</button>
-            <button onClick={this.One} className="button" type="button">1</button><br />
-            <button onClick={this.Zero} className = "button">0</button>
+            <button onClick={() => this.setState({item:this.state.item+'9'})} className = "button" type="button">9</button>
+            <button onClick={() => this.setState({item:this.state.item+'8'})} className = "button">8</button>
+            <button onClick={() => this.setState({item:this.state.item+'7'})}className = "button">7</button><br />
+            <button onClick={() => this.setState({item:this.state.item+'6'})} className = "button">6</button>
+            <button onClick={() => this.setState({item:this.state.item+'5'})}className = "button">5</button>
+            <button onClick={() => this.setState({item:this.state.item+'4'})} className = "button">4</button><br />
+            <button onClick={() => this.setState({item:this.state.item+'3'})} className = "button">3</button>
+            <button onClick={() => this.setState({item:this.state.item+'2'})} className = "button">2</button>
+            <button onClick={() => this.setState({item:this.state.item+'1'})}className="button" type="button">1</button><br />
+            <button className="name">-</button>
+            <button onClick={() => this.setState({item:this.state.item+'0'})} className = "button">0</button>
+            <button className="name">-</button>
         </div>
         </div>
  
